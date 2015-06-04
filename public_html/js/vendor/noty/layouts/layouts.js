4fb84ac6d438bcbf1acbfc3a916bc2e068f5b937
hangarita <hangarita@INV025.edistribution.loc>
2015-06-04 15:32:39 -0500

    Changes

--- public_html/js/vendor/noty/layouts/layouts.js
+++ public_html/js/vendor/noty/layouts/layouts.js
@@ -4,23 +4,6 @@
  * and open the template in the editor.
  */
 
-
-function loadjscssfile(filename, filetype){
-    if (filetype==="js"){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script');
-        fileref.setAttribute("type","text/javascript");
-        fileref.setAttribute("src", filename);
-    }
-    else if (filetype==="css"){ //if filename is an external CSS file
-        var fileref=document.createElement("link");
-        fileref.setAttribute("rel", "stylesheet");
-        fileref.setAttribute("type", "text/css");
-        fileref.setAttribute("href", filename);
-    }
-    if (typeof fileref!=="undefined")
-        document.getElementsByTagName("head")[0].appendChild(fileref);
-}
-
 loadjscssfile("js/vendor/noty/layouts/bottom.js", "js"); //dynamically load and add this .js file
 loadjscssfile("js/vendor/noty/layouts/bottomCenter.js", "js"); //dynamically load "javascript.php" as a JavaScript file
 loadjscssfile("js/vendor/noty/layouts/bottomLeft.js", "js"); //dynamically load "javascript.php" as a JavaScript file
